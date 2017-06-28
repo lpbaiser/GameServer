16efe30500bfb4852a6bf8858cd7b2cc8791deb8
Leonardo <lpbaiser@gmail.com>
2017-06-28 17:47:00 -0300

    mario

--- www/js/screens.js
+++ www/js/screens.js
@@ -63,7 +63,7 @@ class TitleState extends GameState {
         // preparar o jogo
         // reset de highscore e controle de carregamento de estado do servidor
         Config.SCORE = 0
-        Config.LEVEL = 1
+        Config.LEVEL = 2
 
         // evitar bug de levar o callback para outra tela (state)
         this.game.camera.onFadeComplete.removeAll(this)
