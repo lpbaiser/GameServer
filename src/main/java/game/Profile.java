6b1ce3fe834e90009e2a104358b9e89a905d2f79
Leonardo <lpbaiser@gmail.com>
2017-07-05 03:14:31 -0300

    add class Profile

---
+++ src/main/java/game/Profile.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+/**
+ *
+ * @author leonardo
+ */
+public class Profile {
+
+    private Level level;
+    private Player player;
+
+    public Profile(Level level, Player player) {
+        this.level = level;
+        this.player = player;
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public void setLevel(Level level) {
+        this.level = level;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+}
