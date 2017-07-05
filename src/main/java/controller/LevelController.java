e72c7620180e72c1d5104b47ec0de54d75a78f2e
marcocmm <marco.cmm@gmail.com>
2017-07-04 22:24:30 -0300

    .

---
+++ src/main/java/controller/LevelController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import dao.LevelDAO;
+
+/**
+ *
+ * @author leonardo
+ */
+public class LevelController {
+
+    LevelDAO levelDAO;
+
+    public LevelController() {
+        this.levelDAO = new LevelDAO();
+    }
+
+    
+    
+}
